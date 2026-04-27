ef56504d5803d0d2dd48c4e8151cec5909372274
Boomshine01-tech <mcheikh.diop1@univ-thies.sn>
2026-04-27 21:52:24 +0200

    Update Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,19 +8,19 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-/*builder.Services.AddScoped(sp => new HttpClient 
+builder.Services.AddScoped(sp => new HttpClient 
 { 
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
-});*/
+});
 
-builder.Services.AddScoped(sp => new HttpClient
+/*builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(
         builder.HostEnvironment.IsDevelopment()
             ? "http://localhost:8080/"
             : builder.HostEnvironment.BaseAddress
     )
-});
+});*/
 
 
 // Services 
