73919029e1964ec89e8da28a81bd02af53dcd572
Boomshine01-tech <mcheikh.diop1@univ-thies.sn>
2026-07-12 17:31:19 +0200

    Add files via upload

---
+++ Server/Migrations/20260607154052_AddImageUrlToCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETechEnergie.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Categories",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Categories");
+        }
+    }
+}
